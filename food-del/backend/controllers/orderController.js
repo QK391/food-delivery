e069325cc45ee4b88e8faa05269f0700cb2c63d2
kylx <lexuanky391@gmail.com>
2026-03-08 23:21:49 +0700

    add .

--- food-del/backend/controllers/orderController.js
+++ food-del/backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_KEY);
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "http://localhost:5174";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -71,4 +71,24 @@ const userOrders = async (req, res) => {
         res.json({success:false, message: "Error"})
     }
 }
-export {placeOrder, verifyOrder, userOrders}
+//Listing orders
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success: true, data: orders})
+    }catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error"})
+    }
+}
+//api for updating order status
+const updateOrderStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+        res.json({success: true, message: "Order status updated"})
+    }catch (error) {
+        console.log(error);
+        res.json({success:false, message: "Error"})
+    }
+}
+export {placeOrder, verifyOrder, userOrders, listOrders, updateOrderStatus}
